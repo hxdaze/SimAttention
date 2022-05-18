9b351d672d9eab3a82393b140366b0dd3455f99f
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2022-05-18 02:32:22 +0200

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,34 +1,56 @@
 import os
 import torch
 import numpy as np
+from torch.utils.data import Dataset
+from SimAttention.utils.provider import pc_normalize
 
 root = '/home/akira/下载/Pointnet2_PyTorch-master/byol_pcl/data/modelnet40_normal_resampled'
 
 
-class ModelNetDataSet():
-    def __init__(self, root):
+class ModelNetDataSet(Dataset):
+    def __init__(self, root, split='train', cache_size=15000):
         super().__init__()
         self.root = root
         self.catfile = os.path.join(self.root, 'modelnet40_shape_names.txt')
         self.cat = [line.rstrip() for line in open(self.catfile)]
         self.classes = dict(zip(self.cat, range(len(self.cat))))
-        self.split = 'train'
-        self.shape_ids = {'train': [line.rstrip() for line in open(os.path.join(self.root, 'modelnet40_train.txt'))],
-                          'test': [line.rstrip() for line in open(os.path.join(self.root, 'modelnet40_test.txt'))]}
 
-        self.shape_names = ['_'.join(x.split('_')[0:-1]) for x in self.shape_ids[self.split]]
-        self.datapath = [(self.shape_names[i], os.path.join(root, self.shape_names[i], self.shape_ids[self.split][i]) + '.txt')
-                     for i in range(len(self.shape_ids[self.split]))]
+        shape_ids = {'train': [line.rstrip() for line in open(os.path.join(self.root, 'modelnet40_train.txt'))],
+                     'test': [line.rstrip() for line in open(os.path.join(self.root, 'modelnet40_test.txt'))]}
 
-        print(f'{len(self.datapath)} point clouds found.')
+        assert (split == 'train' or split == 'test')
+        shape_names = ['_'.join(x.split('_')[0:-1]) for x in shape_ids[split]]
+        # list of (shape_name, shape_txt_file_path) tuple
+        self.datapath = [(shape_names[i], os.path.join(self.root, shape_names[i], shape_ids[split][i]) + '.txt') for i
+                         in range(len(shape_ids[split]))]
+        print('The size of %s data is %d' % (split, len(self.datapath)))
+
+        self.cache_size = cache_size  # how many data points to cache in memory
+        self.cache = {}  # from index to (point_set, cls) tuple
 
     def __len__(self):
         return len(self.datapath)
 
+    def _get_item(self, index):
+        if index in self.cache:
+            point_set, cls = self.cache[index]
+        else:
+            fn = self.datapath[index]
+            cls = self.classes[self.datapath[index][0]]
+            cls = np.array([cls]).astype(np.int32)
+            point_set = np.loadtxt(fn[1], delimiter=',').astype(np.float32)
+
+            point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
+
+            point_set = point_set[:, 0:3]
+
+            if len(self.cache) < self.cache_size:
+                self.cache[index] = (point_set, cls)
+
+        return point_set, cls
+
     def __getitem__(self, index):
-        path = self.datapath[index][1]
-        point_set = np.loadtxt(path, delimiter=',').astype(np.float32)[:, 0:3]
-        return point_set
+        return self._get_item(index)
 
 
 # data = ModelNetDataSet(root)
@@ -36,8 +58,8 @@ class ModelNetDataSet():
 # flag = 3
 # i = 0
 #
-# for point in DataLoader:
-#     print(point.shape)
+# for point, cls in DataLoader:
+#     print(point.shape, cls)
 #     i += 1
 #     if i == flag:
 #         break
