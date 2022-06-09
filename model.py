174e4085631db4fc1fd66028d44f53d3cbfa3793
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2022-06-09 23:49:19 +0200

    add SimAttn_7
    use target-x-attention, also momentum update

--- model.py
+++ model.py
@@ -8,7 +8,6 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 # loss fn from BYOL
-# todo: loss can be negative, may cause problem
 def loss_fn(x, y):
     x = F.normalize(x, dim=-1, p=2)
     y = F.normalize(y, dim=-1, p=2)
@@ -34,6 +33,9 @@ class ProjectMLP(nn.Module):
 
 
 # crossed attention method:
+# todo: method_1: train 2 different CAs
+# method_2: momentum update the target_CA
+# 50 epoch
 class CrossedAttention(nn.Module):
     def __init__(self, channel=1024):
         super().__init__()
@@ -498,3 +500,186 @@ class SimAttention_5(nn.Module):
 
         return loss.mean()
 
+
+# NS1 without mlp, with tensor methods
+class SimAttention_6(nn.Module):
+    def __init__(self,
+                 aug_function,
+                 sub_function,
+                 slice_function,
+                 cube_function,
+                 sphere_function,
+                 online_encoder,
+                 crossed_attention_method):
+        super().__init__()
+        self.aug_function = aug_function
+        self.sub_function = sub_function
+        self.slice_function = slice_function
+        self.cube_function = cube_function
+        self.sphere_function = sphere_function
+        self.online_encoder = online_encoder
+        self.target_encoder = None
+
+        self.crossed_attention = crossed_attention_method
+
+    def forward(self, x):
+        x = x.to(device)
+        aug1, aug2 = self.aug_function(x), self.aug_function(x)
+
+        sub1, sub2 = self.sub_function(aug1, 1024), self.sub_function(aug2, 1024)
+        slice1, slice2 = self.slice_function(aug1, 1, 1, 1024), self.slice_function(aug2, 1, 1, 1024)
+        cube1, cube2 = self.cube_function(aug1, 0.2, 1024), self.cube_function(aug2, 0.2, 1024)
+        sphere1, sphere2 = self.sphere_function(aug1, 0.2, 1024), self.sphere_function(aug2, 0.1, 1024)
+
+        # [B, 1, N_f] N_f: output dimension of mlp: 512
+        sub_feature_1 = self.online_encoder(sub1)
+        sub_feature_3 = self.online_encoder(sub2)
+
+        # with momentum encoder
+        with torch.no_grad():
+            if self.target_encoder is None:
+                self.target_encoder = copy.deepcopy(self.online_encoder)
+            else:
+                for online_params, target_params in zip(self.online_encoder.parameters(),
+                                                        self.target_encoder.parameters()):
+                    target_weight, online_weight = target_params.data, online_params.data
+                    # moving average decay is tao
+                    tao = 0.99
+                    target_params.data = target_weight * tao + (1 - tao) * online_weight
+            for parameter in self.target_encoder.parameters():
+                parameter.requires_grad = False
+            sub_feature_2 = self.target_encoder(sub2)
+            sub_feature_4 = self.target_encoder(sub1)
+
+        # slice feature [B, 1, N_f]
+        slice_feature_1 = self.online_encoder(slice1)
+        slice_feature_2 = self.online_encoder(slice2)
+
+        # cube feature  [B, 1, N_f]
+        cube_feature_1 = self.online_encoder(cube1)
+        cube_feature_2 = self.online_encoder(cube2)
+
+        # sphere feature [B, 1, N_f]
+        sphere_feature_1 = self.online_encoder(sphere1)
+        sphere_feature_2 = self.online_encoder(sphere2)
+
+        # crop feature concat [B, 3, N_f]
+        crop_feature_1 = torch.cat((slice_feature_1, cube_feature_1, sphere_feature_1), dim=1)
+        crop_feature_2 = torch.cat((slice_feature_2, cube_feature_2, sphere_feature_2), dim=1)
+        # [B, 6, N_f]
+        crop_feature = torch.cat((crop_feature_1, crop_feature_2), dim=1)
+
+        # attention feature
+        attn_feature_1 = self.crossed_attention(sub_feature_1, crop_feature)
+        attn_feature_2 = self.crossed_attention(sub_feature_2, crop_feature)
+        attn_feature_3 = self.crossed_attention(sub_feature_3, crop_feature)
+        attn_feature_4 = self.crossed_attention(sub_feature_4, crop_feature)
+
+        # loss
+        loss_1 = loss_fn(attn_feature_1, attn_feature_2)
+        loss_2 = loss_fn(attn_feature_3, attn_feature_4)
+        loss = loss_1 + loss_2
+
+        return loss.mean()
+
+
+# NS1 with online-target-x-attention methods
+# with online and target crossed attention
+class SimAttention_7(nn.Module):
+    def __init__(self,
+                 aug_function,
+                 sub_function,
+                 slice_function,
+                 cube_function,
+                 sphere_function,
+                 online_encoder,
+                 crossed_attention_method):
+        super().__init__()
+        self.aug_function = aug_function
+        self.sub_function = sub_function
+        self.slice_function = slice_function
+        self.cube_function = cube_function
+        self.sphere_function = sphere_function
+
+        self.online_encoder = online_encoder
+        self.target_encoder = None
+
+        self.online_x_attn = crossed_attention_method
+        self.target_x_attn = None
+
+    def forward(self, x):
+        x = x.cpu().numpy()
+        aug1, aug2 = self.aug_function(x), self.aug_function(x)
+
+        sub1, sub2 = torch.Tensor(self.sub_function(aug1, 1024)).to(device), torch.Tensor(
+            self.sub_function(aug2, 1024)).to(device)
+        slice1, slice2 = torch.Tensor(self.slice_function(aug1, 1, 1, 1024)).to(device), torch.Tensor(
+            self.slice_function(aug2, 1, 1, 1024)).to(device)
+        cube1, cube2 = torch.Tensor(self.cube_function(aug1, 0.2, 1024)).to(device), torch.Tensor(
+            self.cube_function(aug2, 0.2, 1024)).to(device)
+        sphere1, sphere2 = torch.Tensor(self.sphere_function(aug1, 0.2, 1024)).to(device), torch.Tensor(
+            self.sphere_function(aug2, 0.1, 1024)).to(device)
+
+        # [B, 1, N_f] N_f: output dimension of mlp: 512
+        sub_feature_1 = self.online_encoder(sub1)
+        sub_feature_3 = self.online_encoder(sub2)
+
+        # with momentum encoder
+        with torch.no_grad():
+            if self.target_encoder is None:
+                self.target_encoder = copy.deepcopy(self.online_encoder)
+            else:
+                for online_params, target_params in zip(self.online_encoder.parameters(),
+                                                        self.target_encoder.parameters()):
+                    target_weight, online_weight = target_params.data, online_params.data
+                    # moving average decay is tao
+                    tao = 0.99
+                    target_params.data = target_weight * tao + (1 - tao) * online_weight
+            for parameter in self.target_encoder.parameters():
+                parameter.requires_grad = False
+            sub_feature_2 = self.target_encoder(sub2)
+            sub_feature_4 = self.target_encoder(sub1)
+
+        # slice feature [B, 1, N_f]
+        slice_feature_1 = self.online_encoder(slice1)
+        slice_feature_2 = self.online_encoder(slice2)
+
+        # cube feature  [B, 1, N_f]
+        cube_feature_1 = self.online_encoder(cube1)
+        cube_feature_2 = self.online_encoder(cube2)
+
+        # sphere feature [B, 1, N_f]
+        sphere_feature_1 = self.online_encoder(sphere1)
+        sphere_feature_2 = self.online_encoder(sphere2)
+
+        # crop feature concat [B, 3, N_f]
+        crop_feature_1 = torch.cat((slice_feature_1, cube_feature_1, sphere_feature_1), dim=1)
+        crop_feature_2 = torch.cat((slice_feature_2, cube_feature_2, sphere_feature_2), dim=1)
+        # [B, 6, N_f]
+        crop_feature = torch.cat((crop_feature_1, crop_feature_2), dim=1)
+
+        # attention feature
+        with torch.no_grad():
+            if self.target_x_attn is None:
+                self.target_x_attn = copy.deepcopy(self.online_x_attn)
+            else:
+                for online_params, target_params in zip(self.online_x_attn.parameters(),
+                                                        self.target_x_attn.parameters()):
+                    target_weight, online_weight = target_params.data, online_params.data
+                    # moving average decay is tao
+                    tao = 0.99
+                    target_params.data = target_weight * tao + (1 - tao) * online_weight
+            for parameter in self.target_x_attn.parameters():
+                parameter.requires_grad = False
+        # online and target
+        attn_feature_1 = self.online_x_attn(sub_feature_1, crop_feature)
+        attn_feature_2 = self.target_x_attn(sub_feature_2, crop_feature)
+        attn_feature_3 = self.online_x_attn(sub_feature_3, crop_feature)
+        attn_feature_4 = self.target_x_attn(sub_feature_4, crop_feature)
+
+        # loss
+        loss_1 = loss_fn(attn_feature_1, attn_feature_2)
+        loss_2 = loss_fn(attn_feature_3, attn_feature_4)
+        loss = loss_1 + loss_2
+
+        return loss.mean()
