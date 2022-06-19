d9c84b94f349f6e6c0a0aea34f01fade294ad67c
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2022-06-20 01:11:33 +0200

    Update crops.py

--- utils/crops.py
+++ utils/crops.py
@@ -44,6 +44,28 @@ def b_fps(original, npoints):
     return np.array(result)
 
 
+def b_FPS(xyz, npoint):
+    """
+    Input:
+        xyz: pointcloud data, [B, N, 3]
+        npoint: number of samples
+    Return:
+        centroids: sampled pointcloud index, [B, npoint]
+    """
+    device = xyz.device
+    B, N, C = xyz.shape
+    centroids = torch.zeros(B, npoint, dtype=torch.long).to(device)
+    distance = torch.ones(B, N).to(device) * 1e10
+    farthest = torch.randint(0, N, (B,), dtype=torch.long).to(device)
+    batch_indices = torch.arange(B, dtype=torch.long).to(device)
+    for i in range(npoint):
+        centroids[:, i] = farthest
+        centroid = xyz[batch_indices, farthest, :].view(B, 1, 3)
+        dist = torch.sum((xyz - centroid) ** 2, -1)
+        distance = torch.min(distance, dist)
+        farthest = torch.max(distance, -1)[1]
+    return centroids, index_points(xyz, centroids)
+
 # Slice Method
 # patch（3 slices， 40% per slice）
 def get_slice_index(index_slice, ratio_per_slice, overlap_ratio, num_all_points):
