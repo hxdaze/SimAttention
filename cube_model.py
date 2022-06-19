d98eb19feb3e0e2de30c545ec35cb493eba69254
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2022-06-20 01:38:36 +0200

    Update cube_model.py

--- cube_model.py
+++ cube_model.py
@@ -32,8 +32,8 @@ class SimAttention_All_Cubes(nn.Module):
         aug2 = torch.Tensor(self.aug_function(x)).cuda()
 
         # B, 1024, 3
-        sub1 =self.sub_function(aug1, 1024)
-        sub2 = self.sub_function(aug2, 1024)
+       _,  sub1 =self.sub_function(aug1, 1024)
+        _, sub2 = self.sub_function(aug2, 1024)
         
         # B, 8, 1024, 3
         cube1 = self.cube_function(aug1, 8, 0.2, 1024)
