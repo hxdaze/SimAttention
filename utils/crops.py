1896b90c5f4059595cfea92b77653a99f828891a
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2022-06-20 01:37:46 +0200

    Update crops.py

--- utils/crops.py
+++ utils/crops.py
@@ -12,7 +12,6 @@ function 4: get_sphere
 function b: with batch data
 """
 
-
 # FPS Method
 # 需要计算重心坐标的算法
 def fps(original, npoints):
@@ -44,6 +43,20 @@ def b_fps(original, npoints):
     return np.array(result)
 
 
+def index_points(points, idx):
+    """
+    Input:
+        points: input points data, [B, N, C]
+        idx: sample index data, [B, S, [K]]
+    Return:
+        new_points:, indexed points data, [B, S, [K], C]
+    """
+    raw_size = idx.size()
+    idx = idx.reshape(raw_size[0], -1)
+    res = torch.gather(points, 1, idx[..., None].expand(-1, -1, points.size(-1)))
+    return res.reshape(*raw_size, -1)
+
+
 def b_FPS(xyz, npoint):
     """
     Input:
@@ -65,6 +78,33 @@ def b_FPS(xyz, npoint):
         distance = torch.min(distance, dist)
         farthest = torch.max(distance, -1)[1]
     return centroids, index_points(xyz, centroids)
+
+
+def k_points(a, b, k):
+    # a: small, b: big one
+    inner = -2 * torch.matmul(a, b.transpose(2, 1))
+    aa = torch.sum(a**2, dim=2, keepdim=True)
+    bb = torch.sum(b**2, dim=2, keepdim=True)
+    pairwise_distance = -aa - inner - bb.transpose(2, 1)
+    idx = pairwise_distance.topk(k=k, dim=-1)[1]
+    return idx
+
+
+def new_k_patch(x, k=2048, n_patch=8, n_points=1024):
+    patch_centers_index, _ = b_FPS(x, n_patch)  # torch.Size([B, n_patch])
+    center_point_xyz = index_points(x, patch_centers_index)  # [B, n_patch]
+
+    idx = k_points(center_point_xyz, x, k)  # B, k, 2048
+    idx = idx.permute(0, 2, 1)  # B, k, n_patch
+
+    new_patch = torch.zeros([n_patch, x.shape[0], n_points, x.shape[-1]]).to(device)
+    for i in range(n_patch):
+        patch_idx = idx[:, :, i].reshape(x.shape[0], -1)
+        _, patch_points = b_FPS(index_points(x, patch_idx), n_points)
+        new_patch[i] = patch_points
+    new_patch = new_patch.permute(1, 0, 2, 3)   # torch.Size([4, 8, 1024, 3])
+    return new_patch
+
 
 # Slice Method
 # patch（3 slices， 40% per slice）
