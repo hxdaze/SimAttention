405d536636b211697b67c109f35be5f0cd415d0d
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2022-06-23 14:56:31 +0200

    add new function new_k_patch_1024(), no subsample

--- utils/crops.py
+++ utils/crops.py
@@ -106,6 +106,22 @@ def new_k_patch(x, k=2048, n_patch=8, n_points=1024):
     return new_patch
 
 
+def new_k_patch_1024(x, k=1024, n_patch=8, n_points=1024):
+    patch_centers_index, _ = b_FPS(x, n_patch)  # torch.Size([B, n_patch])
+    center_point_xyz = index_points(x, patch_centers_index)  # [B, n_patch]
+
+    idx = k_points(center_point_xyz, x, k)  # B, n_patch, 1024
+    idx = idx.permute(0, 2, 1)  # B, k, n_patch
+
+    new_patch = torch.zeros([n_patch, x.shape[0], n_points, x.shape[-1]]).cuda()
+    for i in range(n_patch):
+        patch_idx = idx[:, :, i].reshape(x.shape[0], -1)
+        patch_points = index_points(x, patch_idx)
+        new_patch[i] = patch_points
+    new_patch = new_patch.permute(1, 0, 2, 3)   # torch.Size([4, 8, 1024, 3])
+    return new_patch
+
+
 # Slice Method
 # patch（3 slices， 40% per slice）
 def get_slice_index(index_slice, ratio_per_slice, overlap_ratio, num_all_points):
