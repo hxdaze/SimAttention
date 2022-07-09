b065acee648d27782910a525995a27820aa8e3f8
Akira2017inFrankfurt <liangindeutschland@gmail.com>
2022-07-09 15:13:45 +0200

    Update train_eval_utils.py

--- utils/train_eval_utils.py
+++ utils/train_eval_utils.py
@@ -1,19 +1,4 @@
 import sys
-
-from tqdm import tqdm
-import torch
-
-from SimAttention.utils.distributed_utils import reduce_value, is_main_process
-from SimAttention.utils.provider import *
-
-
-def train_one_epoch(model, optimizer, data_loader, device, epoch):
-    model.train()
-    mean_loss = torch.zeros(1).to(device)
-    optimizer.zero_grad()
-
-    # 在进程0中打印训练进度
-    if is_main_process():import sys
 import torch
 from tqdm import tqdm
 from utils.provider import *
@@ -30,18 +15,8 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch):
         data_loader = tqdm(data_loader, file=sys.stdout)
 
     for step, data in enumerate(data_loader):
-        points, _ = data
-        # gpu -> cpu
-        points = points.numpy()
-        points = random_point_dropout(points)
-        points = random_scale_point_cloud(points)
-        points = shift_point_cloud(points)
-        # cpu -> gpu
-        points = torch.Tensor(points).cuda()
-        optimizer.zero_grad()
-        # gpu -> cpu
-        # cpu -> gpu
-        loss = model(points)
+        aug1, aug2, _ = data
+        loss = model(aug1, aug2)
         loss = loss.mean()  # 将多个GPU返回的loss取平均
         loss.backward()
         loss = reduce_value(loss, average=True)
@@ -89,65 +64,3 @@ def evaluate(model, data_loader, device):
     sum_num = reduce_value(sum_num, average=False)
 
     return sum_num.item()
-
-        data_loader = tqdm(data_loader, file=sys.stdout)
-
-    for step, data in enumerate(data_loader):
-        points, _ = data
-
-        points = points.numpy()
-        points = random_point_dropout(points)
-        points = random_scale_point_cloud(points)
-        points = shift_point_cloud(points)
-        points = torch.Tensor(points).cuda()
-        optimizer.zero_grad()
-        loss = model(points)
-        loss = loss.mean()  # 将多个GPU返回的loss取平均
-        loss.backward()
-        loss = reduce_value(loss, average=True)
-        mean_loss = (mean_loss * step + loss.detach()) / (step + 1)  # update mean losses
-
-        # 在进程0中打印平均loss
-        if is_main_process():
-            data_loader.desc = "[epoch {}] mean loss {}".format(epoch, round(mean_loss.item(), 3))
-
-        if not torch.isfinite(loss):
-            print('WARNING: non-finite loss, ending training ', loss)
-            sys.exit(1)
-
-        optimizer.step()
-        optimizer.zero_grad()
-
-    # 等待所有进程计算完毕
-    if device != torch.device("cpu"):
-        torch.cuda.synchronize(device)
-
-    return mean_loss.item()
-
-
-@torch.no_grad()
-def evaluate(model, data_loader, device):
-    model.eval()
-    eval_mean_loss = torch.zeros(1).to(device)
-
-    if is_main_process():
-        data_loader = tqdm(data_loader, file=sys.stdout)
-
-    for step, data in enumerate(data_loader):
-        points, _ = data
-        points = points.numpy()
-        points = random_point_dropout(points)
-        points = random_scale_point_cloud(points)
-        points = shift_point_cloud(points)
-        points = torch.Tensor(points).cuda()
-        
-        loss = model(points)
-        loss = loss.mean()  # 将多个GPU返回的loss取平均
-        loss = reduce_value(loss, average=True)
-        eval_mean_loss = (eval_mean_loss * step + loss.detach()) / (step + 1)  # update mean losses
-
-    # 等待所有进程计算完毕
-    if device != torch.device("cpu"):
-        torch.cuda.synchronize(device)
-
-    return eval_mean_loss.item()
